33cb76727941e3b3d480c26d579e23988efed0a3
alexlordville <alexlordville@gmail.com>
2024-06-08 13:15:18 -0700

    Increment read timeout

--- csvTest.py
+++ csvTest.py
@@ -14,18 +14,25 @@ with open("nodesTaskAutom.csv", newline='') as nodesfile:
     for row in nodesReader:
         routerIp = row["ip"]
         device = ConnectHandler(
-        device_type="alcatel_sros",
-        host=routerIp,
-        username="P3194337",
-        password=passwrd,
-    )
-        output = device.send_command("/show system information | match Name\n")
+            device_type="alcatel_sros",
+            host=routerIp,
+            username="P3194337",
+            password=passwrd,
+        )
+        
+        output = device.send_command(
+            "/show system information | match Name\n",
+            read_timeout=90
+        )
+       
         nodeName = output.split()
         today = date.today()
         tsfGen = device.send_command(
-            f"/admin tech-support cf3:{nodeName[3]}.{tsfnumber}",
+            f"/admin tech-support cf3:{nodeName[-1]}.{tsfnumber}",
             expect_string=r"\#",
-            read_timeout=60
+            read_timeout=90
         )
         print(tsfGen)
-        print(f"Tech Support File generated: {routerIp} "+ today.strftime("%A %d. %B %Y"))
+        print(f"Tech Support File generated: {nodeName[-1]}-{routerIp} "+ today.strftime("%A %d. %B %Y"))
+
+         
